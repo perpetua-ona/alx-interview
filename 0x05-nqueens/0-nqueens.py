1baf93619a9e547f2d63fc6385a611c1f1cb7486
root <onaperpetua@gmail.com>
2024-11-19 02:59:05 +0000

    N queen

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''N Queens Challenge'''
+
+import sys
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print('N mus
